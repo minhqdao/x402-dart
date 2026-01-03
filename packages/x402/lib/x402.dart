189dcd9b412a1d75972abd11c2856f393f088ef4
minhqdao <hello@minhdao.de>
2026-01-03 21:10:01 +0700

    Add wrapper package

---
+++ packages/x402/lib/x402.dart
@@ -0,0 +1,7 @@
+/// The x402 package is a unified library that provides support for the x402
+/// protocol across multiple blockchain platforms.
+library;
+
+export 'package:x402_core/x402_core.dart';
+export 'package:x402_evm/x402_evm.dart';
+export 'package:x402_solana/x402_solana.dart';
