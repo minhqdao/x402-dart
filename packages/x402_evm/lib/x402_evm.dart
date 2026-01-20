9507f2069f8084dd0a67d32476fcc901bde07826
minhqdao <hello@minhdao.de>
2026-01-20 22:50:14 +0700

    Restructure src directories

--- packages/x402_evm/lib/x402_evm.dart
+++ packages/x402_evm/lib/x402_evm.dart
@@ -1,4 +1,4 @@
 /// EVM implementation of the x402 payment protocol supporting Ethereum and EVM-compatible chains.
 library;
 
-export 'src/schemes/evm_signer.dart';
+export 'src/signers/evm_signer.dart';
