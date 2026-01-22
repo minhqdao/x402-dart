9041e2e71cfb0439e87f68abf6e2c090de2d5ee9
minhqdao <hello@minhdao.de>
2026-01-22 20:46:39 +0700

    Remove unused class in exact_svm_payload, rename class

--- packages/x402_svm/lib/src/models/exact_svm_payload.dart
+++ packages/x402_svm/lib/src/models/exact_svm_payload.dart
@@ -1,17 +1,15 @@
-import 'package:x402_core/x402_core.dart';
-
 /// SVM transaction data for exact scheme
-class SvmTransactionData {
+class ExactSvmPayload {
   /// Base64-encoded serialized transaction
   final String transaction;
 
   /// Optional: Recent blockhash used
   final String? blockhash;
 
-  const SvmTransactionData({required this.transaction, this.blockhash});
+  const ExactSvmPayload({required this.transaction, this.blockhash});
 
-  factory SvmTransactionData.fromJson(Map<String, dynamic> json) {
-    return SvmTransactionData(
+  factory ExactSvmPayload.fromJson(Map<String, dynamic> json) {
+    return ExactSvmPayload(
         transaction: json['transaction'] as String,
         blockhash: json['blockhash'] as String?);
   }
@@ -21,21 +19,5 @@ class SvmTransactionData {
       'transaction': transaction,
       if (blockhash != null) 'blockhash': blockhash
     };
-  }
-}
-
-/// Parsed exact scheme payload for SVM
-class ExactSvmPayloadData {
-  final SvmTransactionData transactionData;
-
-  const ExactSvmPayloadData({required this.transactionData});
-
-  factory ExactSvmPayloadData.fromPaymentPayload(PaymentPayload payload) {
-    return ExactSvmPayloadData(
-        transactionData: SvmTransactionData.fromJson(payload.payload));
-  }
-
-  Map<String, dynamic> toJson() {
-    return transactionData.toJson();
   }
 }
