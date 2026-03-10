cb39ee35af08cee2cea90d6bf65c87da3488ff5d
minhqdao <hello@minhdao.de>
2026-03-10 18:34:32 +0800

    Remove leftover package.json and revert --esm

--- e2e/ts_client_dart_server/shelf_evm/shelf_evm_test.dart
+++ e2e/ts_client_dart_server/shelf_evm/shelf_evm_test.dart
@@ -83,7 +83,7 @@ void main() {
       // Execute TS client
       final result = await Process.run(
         'npx',
-        ['ts-node', '--esm', 'index.ts'],
+        ['ts-node', 'index.ts'],
         workingDirectory: 'e2e/ts_client_dart_server/shelf_evm',
         environment: {
           ...Platform.environment,
